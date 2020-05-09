58bc6d37a36b943cd60a94f44c6515b75a6dc3bc
Gary Valley <gvalley76@hotmail.com>
2020-05-09 08:32:23 -0700

    Install testing

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ ln -s ~/vim/.vimrc .vimrc
 
 # get Vundle and install all the plugins
 # plugins to be installed are in the ~/vim/.vim/plugins.vim file
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# vim +PluginInstall +qall
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 # bring SpaceViim over and install it
 curl -sLf https://spacevim.org/install.sh | bash
